8931abbdcf9ac9c212ac6460c5bfe8aa5f8a796e
Andrey <61345502+andrey-2-4@users.noreply.github.com>
2023-02-24 15:54:07 +0300

    Add files via upload

--- mark4/mark4.c
+++ mark4/mark4.c
@@ -17,48 +17,111 @@ int isCapital(char s) {
 }
 
 int main(int argc, char *argv[]) {
-	int fd;
-	char buffer[MAX_BUFFER_SIZE];
+	int fd_read_write; // для чтения из файла argv[1] и записи в argv[2]
+	size_t real_string_size = 0; // чтобы понимать какой актуальный размер, т.к. строку мы немного меняем по заданию
 	
-	
-	if ((fd = open(argv[1], O_RDONLY, 0666)) < 0) {
-		printf("coudlnt open the file");
+	int    fd12[2], result12; // для передачи от процесса 1 к процессу 2
+	// открываем канал для процесса 1 и 2
+	if(pipe(fd12) < 0) {
+		printf("Can\'t open pipe\n");
 		exit(-1);
 	}
-	size_t read_bytes = read(fd, buffer, MAX_BUFFER_SIZE);
-	buffer[read_bytes] = '\0';
-	if(close(fd) < 0) {
-		printf("coudlnt close the file");
-		exit(-1);
-	}
-	
-	for (int i = 1; i < sizeof(buffer); ++i) {
-			if (isCapital(buffer[i])) {
-				for (int j = sizeof(buffer) - 1; j > i; --j) {
-					buffer[j] = buffer[j - 1];
+	// пытаемся создать процесс 2
+	result12 = fork();
+	if(result12 < 0) { // если не удалось создать ппроцесс 2
+ 		printf("Can\'t fork child\n");
+ 		exit(-1);
+	} else if (result12 > 0) { /* процесс 1 */
+		char buffer1[MAX_BUFFER_SIZE]; // буффер для процесса 1
+		// процесс 1 читает из файла argv[1]
+		if ((fd_read_write = open(argv[1], O_RDONLY, 0666)) < 0) {
+			printf("coudlnt open the file");
+			exit(-1);
+		}
+		size_t read_bytes = read(fd_read_write, buffer1, MAX_BUFFER_SIZE);
+		buffer1[read_bytes] = '\0';
+		if(close(fd_read_write) < 0) {
+			printf("coudlnt close the file");
+			exit(-1);
+		}
+		// процесс 1 пишет в канал
+		if(close(fd12[0]) < 0){ // закрываем чтение, т.к. процесс 1 только пишет (канал 1 - 2)
+  			printf("parent: Can\'t close reading side of pipe\n"); exit(-1);
+		}
+		write(fd12[1], buffer1, MAX_BUFFER_SIZE);
+		if(close(fd12[1]) < 0) { // закончили писать, закрываем (канал 1 - 2)
+			printf("parent: Can\'t close writing side of pipe\n");
+			exit(-1);
+		}
+	} else { /* процесс 2 */
+		char buffer2[MAX_BUFFER_SIZE]; // буффер для процесса 2
+		// процесс 2 читеат из канала
+		if(close(fd12[1]) < 0){ // закрываем писание, т.к. процесс 2 только читает (канал 1 - 2)
+			printf("child: Can\'t close writing side of pipe\n"); exit(-1);
+		}
+		read(fd12[0], buffer2, MAX_BUFFER_SIZE);
+		if(close(fd12[0]) < 0){ // закончили читать, закрываем (канал 1 - 2)
+			printf("child: Can\'t close reading side of pipe\n"); exit(-1);
+		}
+		// процесс 2 обрабатывает строку
+		for (int i = 1; i < sizeof(buffer2); ++i) {
+			if (isCapital(buffer2[i])) {
+				for (int j = sizeof(buffer2) - 1; j > i; --j) {
+					buffer2[j] = buffer2[j - 1];
 				}
-				buffer[i] = ' ';
+				buffer2[i] = ' ';
 				++i;
 			}
-	}
-	size_t real_string_size = 0;
-	for (int i = 0; i < sizeof(buffer); ++i) {
-		if (buffer[i] == '\0') {
-			break;
 		}
-		real_string_size++;
+		for (int i = 0; i < sizeof(buffer2); ++i) {
+			if (buffer2[i] == '\0') {
+				break;
+			}
+			real_string_size++;
+		}
+		
+		int    fd23[2], result23; // для передачи от процесса 2 к процессу 3
+		// открываем канал для процесса 2 и 3
+		if(pipe(fd23) < 0) {
+			printf("Can\'t open pipe\n");
+			exit(-1);
+		}
+		// пытаемся создать процесс 2
+		result23 = fork();
+		if(result23 < 0) { // если не удалось создать ппроцесс 3
+	 		printf("Can\'t fork child\n");
+	 		exit(-1);
+		} else if (result23 > 0) { /*процесс 2*/
+			// процесс 2 пишет в канал
+			if(close(fd23[0]) < 0){ // закрываем чтение, т.к. процесс 1 только пишет (канал 2 - 3)
+	  			printf("parent: Can\'t close reading side of pipe\n"); exit(-1);
+			}
+			write(fd23[1], buffer2, MAX_BUFFER_SIZE);
+			if(close(fd23[1]) < 0) { // закончили писать, закрываем (канал 1 - 2)
+				printf("parent: Can\'t close writing side of pipe\n");
+				exit(-1);
+			}
+		} else { /*процесс 3*/
+			char buffer3[MAX_BUFFER_SIZE]; // буффер для процесса 3
+			// процесс 3 читеат из канала
+			if(close(fd23[1]) < 0){ // закрываем писание, т.к. процесс 3 только читает (канал 2 - 3)
+				printf("child: Can\'t close writing side of pipe\n"); exit(-1);
+			}
+			read(fd23[0], buffer3, MAX_BUFFER_SIZE);
+			if(close(fd23[0]) < 0){ // закончили читать, закрываем (канал 1 - 2)
+				printf("child: Can\'t close reading side of pipe\n"); exit(-1);
+			}
+			// процесс 3 пишет в argv[2]
+			if ((fd_read_write = open(argv[2], O_WRONLY | O_CREAT, 0666)) < 0) {
+				printf("coudlnt open the file");
+				exit(-1);
+			}
+			write(fd_read_write, buffer3, real_string_size);
+			if (close(fd_read_write) < 0) {
+				printf("coudlnt close the file");
+				exit(-1);
+			}
+		}
 	}
-	
-	
-	if ((fd = open(argv[2], O_WRONLY | O_CREAT, 0666)) < 0) {
-		printf("coudlnt open the file");
-		exit(-1);
-	}
-	write(fd, buffer, real_string_size);
-	if (close(fd) < 0) {
-		printf("coudlnt close the file");
-		exit(-1);
-	}
-	
 	return 0;
 }
