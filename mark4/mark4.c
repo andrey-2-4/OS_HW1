d524f8c70066ef1b16f99a2b2e38aa3fb805e9bb
Andrey <61345502+andrey-2-4@users.noreply.github.com>
2023-02-24 15:09:24 +0300

    Add files via upload

--- mark4/mark4.c
+++ mark4/mark4.c
@@ -5,131 +5,60 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-	// было 4096
-	int size = 5000;
-	// ./a.out input output
-	// input - argv[1] - файл для открытия
-	// output - argv[2] - файл для записи
-	
-  	// чтение
-	int fd;
-	char buffer[size];
-	ssize_t read_bytes;
-	if ((fd = open(argv[1], O_RDONLY)) < 0) {
-		printf("Can\'t open file\n");
-		exit(-1);
-	}
-	read_bytes = read(fd, buffer, size);
-	if (read_bytes == -1) {
-		printf("Can\'t read this file\n");
-		exit(-1);
-	}
-	buffer[read_bytes] = '\0';
-	if (close(fd) < 0) {
-		printf("Can\'t close file\n");
-	}
-	
-	// создаем второй процесс, который будет обрабатывать файл
-	// если это родитель (процесс 1), то мы передаем через НЕИМЕНОВАННЫЙ канал данные ребенку (процессу 2)
-	// иначе наоборот получаем
-	int    fd12[2], result;
-	char   str_buf[read_bytes];
-	
-	if(pipe(fd12) < 0) {
-    		printf("Can\'t open pipe\n");
-    		exit(-1);
-	}
+const int MAX_BUFFER_SIZE = 5000;
 
-	result = fork();
-	if(result < 0) {
-		printf("Can\'t fork child\n");
+int isCapital(char s) {
+	if (s == 'Q' || s == 'W' || s == 'E' || s == 'R' || s == 'T' || s == 'Y' || s == 'U' || s == 'I' || s == 'O'
+	|| s == 'P' || s == 'A' || s == 'D' || s == 'F' || s == 'G' || s == 'H' || s == 'K' || s == 'L' || s == 'Z'
+	|| s == 'X' || s == 'C' || s == 'V' || s == 'B' || s == 'N' || s == 'M' || s == 'S' || s == 'J') {
+		return 1;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	int fd;
+	char buffer[MAX_BUFFER_SIZE];
+	
+	
+	if ((fd = open(argv[1], O_RDONLY, 0666)) < 0) {
+		printf("coudlnt open the file");
 		exit(-1);
-	} else if (result > 0) { /* процесс 1 пишет в канал */
-		if(close(fd12[0]) < 0){
-			printf("parent: Can\'t close reading side of pipe\n"); exit(-1);
-		}
-		size = write(fd12[1], buffer, read_bytes);
-		if(size != read_bytes){
-			printf("Can\'t write all string to pipe\n");
-			 exit(-1);
-		}
-		if(close(fd12[1]) < 0) {
-			printf("parent: Can\'t close writing side of pipe\n");
-			exit(-1);
-		}
-		// printf("Parent exit\n");
-	} else { /* процесс 2 читает из канала, обрабатывает, передает процессу 3 */
-		if(close(fd12[1]) < 0){
-			printf("child: Can\'t close writing side of pipe\n"); exit(-1);
-		}
-		size = read(fd12[0], str_buf, read_bytes);
-		if(size < 0){
-  			printf("Can\'t read string from pipe\n");
-			exit(-1);
-		}
-		// str_buf - это то, что прочитал 2 процесс из канала
-		// printf("Child exit, str_buf: %s\n", str_buf);
-		if(close(fd12[0]) < 0){
-			printf("child: Can\'t close reading side of pipe\n"); exit(-1);
-		}
-		
-		// здесь должна быть обработка
-		
-		// создаем процесс 3
-		if(pipe(fd12) < 0) {
-	    		printf("Can\'t open pipe\n");
-	    		exit(-1);
-		}
-		result = fork();
-		if(result < 0) {
-			printf("Can\'t fork child\n");
-			exit(-1);
-		} else if (result > 0) { /* процесс 2 пишет в канал */
-			if(close(fd12[0]) < 0){
-				printf("parent: Can\'t close reading side of pipe\n"); exit(-1);
-			}
-			size = write(fd12[1], buffer, read_bytes);
-			if(size != read_bytes){
-				printf("Can\'t write all string to pipe\n");
-				 exit(-1);
-			}
-			if(close(fd12[1]) < 0) {
-				printf("parent: Can\'t close writing side of pipe\n");
-				exit(-1);
-			}
-			// printf("Parent exit\n");
-		} else { /* процесс 3 читает из канала, затем пишет в файл*/
-			if(close(fd12[1]) < 0){
-				printf("child: Can\'t close writing side of pipe\n"); exit(-1);
-			}
-			size = read(fd12[0], str_buf, read_bytes);
-			if(size < 0){
-	  			printf("Can\'t read string from pipe\n");
-				exit(-1);
-			}
-			// str_buf - это то, что прочитал 3 процесс из канала
-			// printf("Child exit, str_buf: %s\n", str_buf);
-			if(close(fd12[0]) < 0){
-				printf("child: Can\'t close reading side of pipe\n"); exit(-1);
-			}
-			// запись
-			int len = read_bytes - 1;
-			if ((fd = open(argv[2], O_WRONLY | O_CREAT, 0666)) < 0) {
-				printf("Can\'t open file\n");
-				exit(-1);
-			}
-			size = write(fd, str_buf, len);
-			if (size != len) {
-				printf("Can\'t write all string\n");
-				exit(-1);
-			}
-			if (close(fd) < 0) {
-				printf("Can\'t close file\n");
-			}
-		}
+	}
+	size_t read_bytes = read(fd, buffer, MAX_BUFFER_SIZE);
+	buffer[read_bytes] = '\0';
+	if(close(fd) < 0) {
+		printf("coudlnt close the file");
+		exit(-1);
 	}
 	
+	for (int i = 1; i < sizeof(buffer); ++i) {
+			if (isCapital(buffer[i])) {
+				for (int j = sizeof(buffer) - 1; j > i; --j) {
+					buffer[j] = buffer[j - 1];
+				}
+				buffer[i] = ' ';
+				++i;
+			}
+	}
+	size_t real_string_size = 0;
+	for (int i = 0; i < sizeof(buffer); ++i) {
+		if (buffer[i] == '\0') {
+			break;
+		}
+		real_string_size++;
+	}
+	
+	
+	if ((fd = open(argv[2], O_WRONLY | O_CREAT, 0666)) < 0) {
+		printf("coudlnt open the file");
+		exit(-1);
+	}
+	write(fd, buffer, real_string_size);
+	if (close(fd) < 0) {
+		printf("coudlnt close the file");
+		exit(-1);
+	}
 	
 	return 0;
 }
